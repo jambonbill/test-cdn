ad5ba0bdb35a4a953922dc095d0ee8e90b6455f7
jambonbill <jambonbill@gmail.com>
2023-12-07 11:04:03 +0100

    gitignore

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # test-cdn
-un test
+
+Un test de CDN, avant de passer aux choses serieuses
+
+- lol
+- cool
 
 
 https://adrianmanduc.medium.com/how-to-create-a-js-library-and-publish-it-to-npm-6e6351971984
-
---
+++ src/index.js
@@ -0,0 +1,7 @@
+export const capitalize = word => word.charAt(0).toUpperCase() + word.slice(1);
+
+export const taratata = word => word.charAt(0).toUpperCase() + word.slice(1);
+
+export const rnd = n => Math.floor(Math.random()*n)
+
+//console.log("LOL COOL","😍😘")
